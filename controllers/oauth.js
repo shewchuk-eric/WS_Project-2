50d6ccb722d7cc831fb4e1e0b4ee450a2ba9baa7
Eric Shewchuk <temeculastoneworks@msn.com>
2025-10-31 12:33:20 -1000

    got oauth working

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -1,39 +1,56 @@
-//const passport = require('passport');
-//const session = require('express-session');
-//const GitHubStrategy = require('passport-github2').Strategy;
-//require('dotenv').config(); 
+/*require("dotenv").config();
 
-/*passport.use(new GitHubStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/github/callback" 
-  }, (accessToken, refreshToken, profile, done) => {
-    return done(null, profile);
-  }));
-.use(session({
+const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const GithubStrategy = require("passport-github2").Strategy;
+
+const app = express();
+
+app.use(
+  session({
     secret: "secret",
-    resave: false, 
-    saveUninitialized: true
-  }))
-  //.use(passport.initialize())
-  //.use(passport.session())
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/github/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
 );
 
 passport.serializeUser((user, done) => {
+  console.log('In serializeUser');
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
-}); */
 
+//const app = (require('../index.js'));
+//const passport = (require('../index.js'));
+*/
 
 
 const login = async (req, res, next) => {
-    res.send('<a href="/github/auth">Login With GitHub<a>');
+    res.send('<a href="/github/auth">Login With Your GitHub Account<a>');
   };
 
 const authorize = async (req, res, next) => {
+  console.log('In authorize');
     passport.authenticate('github', { scope: [ 'user:email' ] })
 };
 
--- index.js
+++ index.js
@@ -7,12 +7,33 @@ const session = require('express-session');
 const passport = require('passport');
 const app = express();
 const GitHubStrategy = require('passport-github2').Strategy;
-//const cookieParser = require('cookie-parser'); // Uncomment if cookie parsing is needed
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const port = process.env.PORT || 3000; 
 
 require('dotenv').config(); 
+
+
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .use(session({
+    secret: "secret",
+    resave: false, 
+    saveUninitialized: true
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') return res.sendStatus(204);
+    next();
+  })
+  .use('/', require('./routes/index.js'));
 
 passport.use(new GitHubStrategy({
     clientID:process.env.CLIENT_ID,
@@ -30,40 +51,22 @@ passport.deserializeUser((obj, done) => {
   done(null, obj);
 });
 
-app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use(cors())
-  .use(express.json())
-  .use(express.urlencoded({ extended: true }))
-  .use(session({
-    secret: "secret",
-    resave: false, 
-    saveUninitialized: true
-  }))
-  .use(passport.initialize())
-  .use(passport.session())
-  //.use(cookieParser()) // Uncomment if cookie parsing is needed
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') return res.sendStatus(204);
-    next();
-  })
-  .use('/', require('./routes/index.js'));
-
   /**********************************************
    * tutorial code follows *
    **********************************************/
 
 app.get("/", (req, res) => {
-  res.send('<a href="/auth/github">Login with Github</a>');
-});
+  if(req.user) {res.send('<a href="/api-docs/#/">You are logged in. View Swagger docs</a>')
+  } else {
+  //res.redirect('github/');
+  res.send('<a href="/auth/github">Login with GitHub</a>');
+}});
 
-app.get("/auth/github", passport.authenticate('github')); //, { scope: ["user:email"] }
+app.get("/auth/github", passport.authenticate("github", { scope: ["user:email"] }));
 
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/login' }),
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
   (req, res) => {
     res.redirect("/api-docs/#/");
   }
@@ -92,3 +95,5 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+module.exports = { app, passport };
